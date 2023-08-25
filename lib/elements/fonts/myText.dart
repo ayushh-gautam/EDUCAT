94cc578e18afeb6f8ab667b650c1220179abf4f8
ayushh-gautam <ayushgautam896@gmail.com>
2023-08-25 21:45:57 +0545

    hehe

--- lib/elements/fonts/myText.dart
+++ lib/elements/fonts/myText.dart
@@ -4,34 +4,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 // ignore: must_be_immutable
 class MyText extends StatelessWidget {
-  TextAlign? textAlign;
-  String text;
-  double fontSize;
-  Color? color;
-  double? letterSpacing;
-  FontWeight? fontWeight;
-  MyText({
-    Key? key,
-    this.textAlign,
-    required this.text,
-    required this.fontSize,
-    this.color,
-    this.letterSpacing,
-    this.fontWeight,
-  }) : super(key: key);
+  const MyText({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      textAlign: textAlign,
-      style: GoogleFonts.workSans(
-        textStyle: TextStyle(
-            letterSpacing: letterSpacing,
-            color: color,
-            fontSize: fontSize,
-            fontWeight: fontWeight),
-      ),
-    );
+    return const Placeholder();
   }
 }
